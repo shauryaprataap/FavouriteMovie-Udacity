7aa3d5f884e84c33c8e1f21f908262dec183dad9
shauryaprataap <theshauryapratap@gmail.com>
2020-06-02 23:37:35 +0530

    Udacity

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Dashboard from "./Dashboard";
 /*
 Display a list of movies where each movie contains a list of users that favorited it.
 
@@ -11,93 +11,108 @@ For detailed instructions, refer to instructions.md.
 const profiles = [
   {
     id: 1,
-    userID: '1',
-    favoriteMovieID: '1',
+    userID: "1",
+    favoriteMovieID: "1",
   },
   {
     id: 2,
-    userID: '2',
-    favoriteMovieID: '1',
+    userID: "2",
+    favoriteMovieID: "1",
   },
   {
     id: 3,
-    userID: '4',
-    favoriteMovieID: '5',
+    userID: "4",
+    favoriteMovieID: "5",
   },
   {
     id: 4,
-    userID: '5',
-    favoriteMovieID: '2',
+    userID: "5",
+    favoriteMovieID: "2",
   },
   {
     id: 5,
-    userID: '3',
-    favoriteMovieID: '5',
+    userID: "3",
+    favoriteMovieID: "5",
   },
   {
     id: 6,
-    userID: '6',
-    favoriteMovieID: '4',
+    userID: "6",
+    favoriteMovieID: "4",
   },
 ];
 
 const users = {
   1: {
     id: 1,
-    name: 'Jane Jones',
-    userName: 'coder',
+    name: "Jane Jones",
+    userName: "coder",
   },
   2: {
     id: 2,
-    name: 'Matthew Johnson',
-    userName: 'mpage',
+    name: "Matthew Johnson",
+    userName: "mpage",
   },
   3: {
     id: 3,
-    name: 'Autumn Green',
-    userName: 'user123',
+    name: "Autumn Green",
+    userName: "user123",
   },
   4: {
     id: 3,
-    name: 'John Doe',
-    userName: 'user123',
+    name: "John Doe",
+    userName: "user123",
   },
   5: {
     id: 5,
-    name: 'Lauren Carlson',
-    userName: 'user123',
+    name: "Lauren Carlson",
+    userName: "user123",
   },
   6: {
     id: 6,
-    name: 'Nicholas Lain',
-    userName: 'user123',
+    name: "Nicholas Lain",
+    userName: "user123",
   },
 };
 
 const movies = {
   1: {
     id: 1,
-    name: 'Planet Earth',
+    name: "Planet Earth",
   },
   2: {
     id: 2,
-    name: 'Selma',
+    name: "Selma",
   },
   3: {
     id: 3,
-    name: 'Million Dollar Baby',
+    name: "Million Dollar Baby",
   },
   4: {
     id: 4,
-    name: 'Forrest Gump',
+    name: "Forrest Gump",
   },
   5: {
     id: 5,
-    name: 'Get Out',
+    name: "Get Out",
   },
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.usersByMovie = {};
+
+    profiles.forEach((profile) => {
+      const movieId = profile.favoriteMovieID;
+
+      if (this.usersByMovie[movieId]) {
+        this.usersByMovie[movieId].push(profile.userID);
+      } else {
+        this.usersByMovie[movieId] = [profile.userID];
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -106,6 +121,12 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>How Popular is Your Favorite Movie?</h2>
+        <Dashboard
+          profiles={profiles}
+          movies={movies}
+          users={users}
+          usersByMovie={this.usersByMovie}
+        />
       </div>
     );
   }
--- src/MovieCard.js
+++ src/MovieCard.js
@@ -2,11 +2,6 @@ import React, { Component } from "react";
 
 class MovieCard extends Component {
   render() {
-    /*
-    Destructuring via ES6. We're getting the profiles, users, and movies properties
-    off of the pros passed into this presentational component. If you need a refresher on this syntax, check
-    out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
-    */
     const { users, usersWhoLikedMovie, movieInfo } = this.props;
 
     return (
@@ -15,7 +10,7 @@ class MovieCard extends Component {
         <h3>Liked By:</h3>
 
         {!usersWhoLikedMovie || usersWhoLikedMovie.length === 0 ? (
-          <p>None of the current users liked this movie.</p>
+          <p>None of the user liked movies</p>
         ) : (
           <ul>
             {usersWhoLikedMovie &&
